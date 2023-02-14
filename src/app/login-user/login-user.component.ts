de854115defe451ac745d1b548de25f71e34475b
fakihibrohim <fakihibrohin37@gmail.com>
2023-02-15 00:38:38 +0700

    first commit

---
+++ src/app/login-user/login-user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-login-user',
+  templateUrl: './login-user.component.html',
+  styleUrls: ['./login-user.component.css']
+})
+export class LoginUserComponent {
+
+  user:User = new User();
+
+}
